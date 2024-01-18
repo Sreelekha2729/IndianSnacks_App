f92cfbee2a7ccb0b8c4711fe6046293558816749
Sree Lekha <sreelekha2729@gmail.com>
2024-01-18 19:38:00 +0530

    Made few changes

--- loginapp/app.js
+++ loginapp/app.js
@@ -5,15 +5,15 @@ const db = require("./db.js");
 const dotenv = require("dotenv");
 dotenv.config();
 const path = require("path");
-// const port = process.env.PORT || 5001;
+const port = process.env.PORT || 8080;
 const AuthController = require("./controller/authController");
 
 app.use(cors());
 app.use("/api/auth", AuthController);
 
-//static files
+//static folders
 app.use(express.static(path.join(__dirname, "./client/build")));
-  
+//static routes
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
